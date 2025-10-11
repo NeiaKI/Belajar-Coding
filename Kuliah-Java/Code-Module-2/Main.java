ce7ba7c901ade44ecc97edd1edb7c78f0842d59a
NEki <febieki562@gmail.com>
2025-10-11 21:08:56 +0700

    memperbarui

--- Kuliah-Java/Code-Module-2/Main.java
+++ Kuliah-Java/Code-Module-2/Main.java
@@ -1,14 +1,14 @@
 public class Main {
-public static void main(String[] args) {
-int umur = 21;
-double ipk = 3.75;
-char grade = 'A';
-boolean aktif = true;
-String nama = "Budi";
-System.out.println("Nama : " + nama);
-System.out.println("Umur : " + umur);
-System.out.println("IPK : " + ipk);
-System.out.println("Grade : " + grade);
-System.out.println("Aktif : " + aktif);
-}
+    public static void main(String[] args) {
+    int umur = 21;
+    double ipk = 3.75;
+    char grade = 'A';
+    boolean aktif = true;
+    String nama = "Budi";
+    System.out.println("Nama    : " + nama);
+    System.out.println("Umur    : " + umur);
+    System.out.println("IPK     : " + ipk);
+    System.out.println("Grade   : " + grade);
+    System.out.println("Aktif   : " + aktif);
+    }
 }
